585e6dd6151e29319b1fa215f25bc1280e72a6fe
Igor Klopov <igor@klopov.com>
2017-05-19 01:09:19 +0300

    0.0.4

--- postinstall/index.js
+++ postinstall/index.js
@@ -6,14 +6,19 @@ import path from 'path';
 import { plusx } from './chmod.js';
 
 const latest = 'https://now-cli-latest.zeit.sh/';
+const platformToName = {
+  darwin: 'now-macos',
+  linux: 'now-linux'
+};
 
 async function main () {
   info(`Retrieving ${latest}...`);
   let resp = await fetch(latest);
   if (resp.status !== 200) throw new Error(resp.statusText);
   const json = await resp.json();
-  const asset = json.assets[2];
-  const { name, url } = asset;
+  const name = platformToName[process.platform];
+  const asset = json.assets.filter((a) => a.name === name)[0];
+  const { url } = asset;
   resp = await fetch(url);
   const size = resp.headers.get('content-length');
   const file = path.join(__dirname, 'now');
