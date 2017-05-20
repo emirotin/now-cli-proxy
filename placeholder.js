dc20c384da87141aab66f66bb680c379a6272bec
Igor Klopov <igor@klopov.com>
2017-05-20 23:12:22 +0300

    Revert "placeholder.js to populate 'now.exe' with 'interrupted' message"
    This reverts commit 25f94857245146f05196ed09f741a4e070e585fc.

--- placeholder.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-var path = require('path')
-var dist = path.join(__dirname, 'dist')
-var now = path.join(dist, 'now.exe')
-
-fs.writeFileSync(now,
-  '#!/usr/bin/env node\n' +
-  'console.log("\'Now\' binary downloading was interrupted. Please reinstall!")\n'
-)
