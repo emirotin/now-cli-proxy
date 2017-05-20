147754c21fc08e22db5014cdd686f36a8cdd3bb3
Igor Klopov <igor@klopov.com>
2017-05-20 18:28:52 +0300

    download to '.partial' file, and rename upon success

--- download/chmod.js
+++ download/chmod.js
@@ -1,27 +1,9 @@
 import fs from 'fs'
 
-function stat (file) {
-  return new Promise((resolve, reject) => {
-    fs.stat(file, (error, stats) => {
-      if (error) return reject(error)
-      resolve(stats)
-    })
-  })
-}
-
-function chmod (file, mode) {
-  return new Promise((resolve, reject) => {
-    fs.chmod(file, mode, (error) => {
-      if (error) return reject(error)
-      resolve()
-    })
-  })
-}
-
-export async function plusx (file) {
-  const s = await stat(file)
+export function plusxSync (file) {
+  const s = fs.statSync(file)
   const newMode = s.mode | 64 | 8 | 1
   if (s.mode === newMode) return
   const base8 = newMode.toString(8).slice(-3)
-  await chmod(file, base8)
+  fs.chmodSync(file, base8)
 }
--- download/index.js
+++ download/index.js
@@ -2,17 +2,12 @@ import { disableProgress, enableProgress, showProgress } from './log.js'
 import fetch from 'node-fetch'
 import fs from 'fs'
 import path from 'path'
-import { plusx } from './chmod.js'
+import { plusxSync } from './chmod.js'
 
-const target = 'now.exe'
+const target = path.join(__dirname, 'now.exe')
+const partial = target + '.partial'
 const packageJson = path.join(__dirname, '../package.json')
 const { version } = JSON.parse(fs.readFileSync(packageJson, 'utf8'))
-
-const platformToName = {
-  darwin: 'now-macos',
-  linux: 'now-linux',
-  win32: 'now-win.exe'
-}
 
 const names = {
   darwin: {
@@ -29,17 +24,16 @@ const names = {
   }
 }
 
-async function main() {
+async function main () {
   const nameDetails = names[process.platform]
   const url = `https://github.com/zeit/now-cli/releases/download/${version}/${nameDetails.binary}`
 
-  const resp = await fetch(url)
-  const size = resp.headers.get('content-length')
-  const file = path.join(__dirname, target)
-  const ws = fs.createWriteStream(file)
-
   enableProgress('Downloading `now` for ' + nameDetails.platform)
   showProgress(0)
+
+  const resp = await fetch(url)
+  const size = resp.headers.get('content-length')
+  const ws = fs.createWriteStream(partial)
 
   await new Promise((resolve, reject) => {
     let bytesRead = 0
@@ -61,7 +55,8 @@ async function main() {
     })
   })
 
-  await plusx(file)
+  fs.renameSync(partial, target)
+  plusxSync(target)
 }
 
 main().catch(error => {
